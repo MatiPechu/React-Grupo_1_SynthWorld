5ad9b32fe5a4892e302e961db7300fb44af842cd
Vicky <mvictoriaripa@gmail.com>
2023-03-05 18:05:07 +0100

    agregado de footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,16 @@
 import React from "react";
+import '../assets/css/Footer.css'
+
+
+function Footer() {
+    return (
+        <React.Fragment>
+            <footer className="contenedor">
+						<span className = "texto">Copyright &copy; Synthworld </span>
+			</footer>
+
+        </React.Fragment>
+
+    )
+}
+export default Footer;
