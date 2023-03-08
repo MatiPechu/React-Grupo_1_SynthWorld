68d32aab2dacbb95bda057c5fb3c1d6144882e87
AgusGol <agustingolberg@gmail.com>
2023-03-08 18:33:03 +0100

    agregsando la foto y el brand del product

--- src/components/CardTotal.js
+++ src/components/CardTotal.js
@@ -4,7 +4,7 @@ import '../assets/css/CardTotal.css'
 function Card(props){
     return(
 <div className="content totals">
-    <h3> Total de {props.name}</h3>
+    <h3> Total {props.name}</h3>
         <div className='total'>
             <p>{props.data}</p>
             <i className={props.icon}></i>
