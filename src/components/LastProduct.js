1dff0e716a76f6d900001478efb953fc35f260a4
Vicky <mvictoriaripa@gmail.com>
2023-03-08 20:04:22 +0100

    corrección de textos español a inglés

--- src/components/LastProduct.js
+++ src/components/LastProduct.js
@@ -17,7 +17,7 @@ function LastProduct(props){
 
     return(
 <div className="content lastProduct">
-    <h3>Ultimo producto creado</h3>
+    <h3>Last created product</h3>
         <div className='productNamePrice'>
             <p>{props.product.name}</p>
             <p>${props.product.price}</p>
