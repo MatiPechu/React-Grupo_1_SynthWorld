df22d340eee4ef6ff093224c0973dfcd83b4269b
Vicky <mvictoriaripa@gmail.com>
2023-03-08 02:39:49 +0100

    Agregado de SideBar. Versión casi definitiva :')

--- src/components/Estructura.js
+++ src/components/Estructura.js
@@ -4,11 +4,13 @@ import Banner1 from "./Banner1";
 import Banner2 from "./Banner2";
 import ContentTotally from "./ContentTotally";
 import Footer from "./Footer";
+import Sidebar from "./SideBar";
 
 function Estructura(){
     return(
 <>
 <Navbar/>
+<Sidebar/>
 <div>
     <Banner1/>
 <div className="total-panels">
---
+++ src/components/SideBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../assets/css/SideBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faBoxOpen, faTags, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <div className="sidebar-title">Welcome back</div>
+      </div>
+      <ul className="sidebar-menu">
+        <li className="sidebar-menu-item">
+          <FontAwesomeIcon icon={faUsers} className="sidebar-menu-icon" />
+          Users
+        </li>
+        <li className="sidebar-menu-item">
+          <FontAwesomeIcon icon={faBoxOpen} className="sidebar-menu-icon" />
+          Products
+        </li>
+        <li className="sidebar-menu-item">
+          <FontAwesomeIcon icon={faTags} className="sidebar-menu-icon" />
+          Brands
+        </li>
+        <li className="sidebar-menu-item">
+          <FontAwesomeIcon icon={faCog} className="sidebar-menu-icon" />
+          Configuration
+        </li>
+        <li className="sidebar-menu-item">
+          <FontAwesomeIcon icon={faSignOutAlt} className="sidebar-menu-icon" />
+          Log Out
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
